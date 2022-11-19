75a6f7fb62d980f133d3429afac77073aec49b54
gersonkaitor <gersonkaitor@gmail.com>
2022-11-19 17:34:28 +0800

    user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserResult from "../components/users/UserResult"
+import UserSearch from "../components/users/UserSearch"
 
 function Home() {
   return (
     <>
-      {/* serach component */}
+      <UserSearch/>
       <UserResult/>
     </>
   )
