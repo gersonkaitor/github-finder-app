d5c6681a415d49cf25368ebc97448a0af1f1ad74
gersonkaitor <gersonkaitor@gmail.com>
2022-11-23 10:37:58 +0800

    change text color

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -42,21 +42,21 @@ function User() {
 
   return (
     <>
-      <div className="w-full mx-auto lg:w-10/12">
+      <div className="w-full mx-auto lg:w-10/12 text-primary-content">
         <div className="mb-4">
           <Link to="/" className="btn btn-ghost">
             Back To Search
           </Link>
         </div>
         <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
-          <div className="custom-card-image mb-6 md:mb-0">
+          <div className="custom-card-image mb-6 md:mb-0 ">
             <div className="rounded-lg shadow-xl card image-full">
               <figure>
                 <img src={avatar_url} alt="profile_image" />
               </figure>
               <div className="card-body justify-end">
-                <h2 className="card-title mb-0">{name}</h2>
-                <p className="flex-grow-0">{login}</p>
+                <h2 className="card-title mb-0 text-primary-content">{name}</h2>
+                <p className="flex-grow-0 text-primary-content">{login}</p>
               </div>
             </div>
           </div>
@@ -84,7 +84,7 @@ function User() {
                 </div>
             </div>
 
-            <div className="w-full rounded-lg shadow-md bg-base-100 stats stats-vertical lg:stats-horizontal">
+            <div className="w-full rounded-lg shadow-md bg-base-100 text-primary-content stats stats-vertical lg:stats-horizontal">
             {location && (
                 <div className='stat'>
                   <div className='stat-title text-md'>Location</div>
@@ -114,7 +114,7 @@ function User() {
             </div>
           </div>
         </div>
-        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 text-primary-content stats stats-vertical lg:stats-horizontal">
 
           <div className="stat">
             <div className="stat-figure text-secondary">
